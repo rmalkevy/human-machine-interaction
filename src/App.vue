277b32595fc6c21ddcc6182690c677b1311cfe55
Malkevych Roman <henprokuror@gmail.com>
2025-09-24 18:39:07 +0300

    in progress

--- src/App.vue
+++ src/App.vue
@@ -3,86 +3,70 @@ import './reset.css';
 
 import { defineModel, computed, watch, ref, onMounted, onUnmounted } from 'vue';
 
-const counterModel = defineModel('counterModel', { type: Number, default: 0 });
-const tempRef = ref(0);
-
-const handleClick = () => {
-  counterModel.value++;
-};
-
-const computedLabel = computed(() => `Counter: ${counterModel.value}`);
-
-watch(counterModel, (newVal) => {
-  localStorage.setItem('counterModel', newVal);
-  console.log('counterModel changed = ', newVal, 'tempRef = ', tempRef.value);
-});
+const snakeGameBoard = ref([]);
+const snake = ref([])
 
 onMounted(() => {
-  counterModel.value = Number(localStorage.getItem('counterModel'));
-})
+  snakeGameBoard.value = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => 0));
+  snake.value = [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 2, y: 0 },
+  ];
+  console.log(snakeGameBoard.value);
+});
 
-onUnmounted(() => {
-  console.log('onUnmounted');
-})
 </script>
 
 <template>
-  <div class="kai-container">
-    <button class="kai-button" @click="handleClick">{{ computedLabel }}</button>
-    <input type="number" class="kai-input" v-model="counterModel" id="kai-input" />
+  <div class="snake-game-container">
+    <div class="snake-game-board">
+      <div class="snake-game-board-row" v-for="row, rowIndex in snakeGameBoard" :key="`row-${rowIndex}`">
+        <div
+          v-for="col, colIndex in row"
+          class="snake-game-board-cell"
+          :class="{ 'snake-game-board-cell-snake': snake.some(item => item.x === colIndex && item.y === rowIndex) }"
+          :key="`row-${rowIndex}-col-${colIndex}`"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.kai-button {
-  background-color: blue;
-  color: #fff;
-  height: 30px;
-  border-radius: 5px;
-  width: 100px;
-  cursor: pointer;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #ccc;
-  padding: 0 10px;
-
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
-  transition: box-shadow 0.3s ease;
-
-  &:hover {
-    box-shadow: 0 8px 12px rgba(0, 0, 0, 0.3);
-  }
-}
-
-.kai-input {
-  width: 100px;
-  height: 30px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  padding: 0 10px;
-
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
-  transition: box-shadow 0.3s ease;
-
-  &:hover {
-    box-shadow: 0 8px 12px rgba(0, 0, 0, 0.3);
-  }
-
-  &:focus {
-    border: 1px solid blue;
-  }
-}
-
-.kai-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
+.snake-game-container {
   width: 100vw;
   height: 100vh;
-  background-color: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.snake-game-board {
+  width: 500px;
+  height: 500px;
+  background-color: #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 1px solid #000;
+}
+.snake-game-board-row {
+  width: 100%;
+  height: 50px;
+  display: flex;
+}
+.snake-game-board-cell {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+.snake-game-board-cell + .snake-game-board-cell {
+  border-left: 1px solid #000;
+}
+.snake-game-board-row + .snake-game-board-row {
+  border-top: 1px solid #000;
+}
+.snake-game-board-cell-snake {
+  background-color: rgba(255, 0, 0, 0.716);
 }
 </style>
